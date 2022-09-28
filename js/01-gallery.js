b451189d35246c358379efb30a976ee94fd00ac7
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-28 19:56:21 +0300

    Додає модальне вікно з використанням бібліотеки basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,35 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-
 const gallery = document.querySelector(".gallery");
 
 gallery.innerHTML = addGalleryItems(galleryItems);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
+
+  instance.show();
+
+  document.addEventListener("keydown", closeLightboxEscape);
+});
+
+basicLightboxEl.addEventListener("click", (event) => {
+  console.log("asdas");
+  stopListenerKeydown();
+});
 
 function addGalleryItems(items) {
   return items
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -23,4 +42,8 @@ function addGalleryItems(items) {
     .join("");
 }
 
-
+function closeLightboxEscape() {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
