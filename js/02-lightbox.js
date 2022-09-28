7ee127172871242220f2b27955aa54fa2f530263
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-28 23:35:18 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,24 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
 
 gallery.innerHTML = addGalleryItems(galleryItems);
 
-var lightbox = new SimpleLightbox('.gallery__item', {
-    enableKeyboard: true,
-    captionDelay: 250,
-   
+var lightbox = new SimpleLightbox(".gallery__item", {
+  enableKeyboard: true,
+  captionDelay: 250,
 });
 
-console.log(lightbox)
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
 
   if (event.target.nodeName !== "IMG") {
     return;
   }
-    
-    lightbox.overlay = true;
-});
 
+  lightbox.overlay = true;
+});
 
 function addGalleryItems(items) {
   return items
